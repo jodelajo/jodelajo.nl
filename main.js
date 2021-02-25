568113c942015c6f576c3d6c1c1f7d6aa3b16986
jodelajo <jodelajo@gmail.com>
2021-02-25 10:42:16 +0100

    start page

---
+++ main.js
@@ -0,0 +1,14 @@
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+    let i;
+    const x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+}
