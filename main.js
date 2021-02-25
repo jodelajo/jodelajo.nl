67d9ce30bec8e15ff5e6a601ec5baef1664b6b8d
jodelajo <jodelajo@gmail.com>
2021-02-25 16:57:29 +0100

    super clean hamburger css

--- main.js
+++ main.js
@@ -12,3 +12,13 @@ function carousel() {
     x[slideIndex-1].style.display = "block";
     setTimeout(carousel, 4000); // Change image every 2 seconds
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function responsiveMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
