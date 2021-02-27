ad604679e4873ba6067b886ddf4669f61f5b5861
jodelajo <jodelajo@gmail.com>
2021-02-27 12:34:06 +0100

    responsiver

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-let slideIndex = 0;
+
 carousel();
 
 function carousel() {
+    var slideIndex = 0;
     let i;
-    const x = document.getElementsByClassName("mySlides");
+    var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
+    if (slideIndex > x.length) {
+        slideIndex = 1}
     x[slideIndex-1].style.display = "block";
     setTimeout(carousel, 4000);
 }
